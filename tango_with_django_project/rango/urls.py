07f07d9c07603b297b7e163ef915429ecece7df8
gzpgg3x <gzpgg3x@yahoo.com>
2014-04-22 18:01:15 -0400

    6.3. Creating a Details Page
    04220140600pm
    "about" is missing

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, url
 from rango import views
 
+# urlpatterns = patterns('',
+#         url(r'^$', views.index, name='index'))
+
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+    url(r'^$', views.index, name='index'),
+    #url(r'^about/$', views.about, name='about'),
+    url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),)  # New!
