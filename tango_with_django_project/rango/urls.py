3f1191f2f569529ad56dd46725898933373f933e
gzpgg3x <gzpgg3x@yahoo.com>
2014-04-24 00:20:34 -0400

    8.5. Adding Login Functionality ~ 8.5.5. Demo
    042420141220AM

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     #url(r'^about/$', views.about, name='about'),
     url(r'^add_category/$', views.add_category, name='add_category'), # NEW MAPPING!
     url(r'^category/(?P<category_name_url>\w+)/add_page/$', views.add_page, name='add_page'), 
-    url(r'^register/$', views.register, name='register'), # ADD NEW PATTERN!   
+    url(r'^register/$', views.register, name='register'), # ADD NEW PATTERN! 
+    url(r'^login/$', views.user_login, name='login'),  
     url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),)  # New!
