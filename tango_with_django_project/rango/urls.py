a2c151baaa5a647c3538705bf4fe28ff4c4421b6
gzpgg3x <gzpgg3x@yahoo.com>
2014-04-27 04:11:33 -0400

    15. AJAX, Django and JQuery ~ 15.1.5. Making the AJAX request
    NOT WORK
    042720140411a

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -17,5 +17,6 @@ urlpatterns = patterns('',
     url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^goto/$', views.track_url, name='track_url'),
+    url(r'^like_category/$', views.like_category, name='like_category'),
     # url(r'^search/$', views.search, name='search'),
     ) 
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -398,6 +398,23 @@ def track_url(request):
             except:
                 pass
 
-    return redirect(url)                  
+    return redirect(url)     
+
+@login_required
+def like_category(request):
+    context = RequestContext(request)
+    cat_id = None
+    if request.method == 'GET':
+        cat_id = request.GET['category_id']
+
+    likes = 0
+    if cat_id:
+        category = Category.objects.get(id=int(cat_id))
+        if category:
+            likes = category.likes + 1
+            category.likes = likes
+            category.save()
+
+    return HttpResponse(likes)              
 
           
